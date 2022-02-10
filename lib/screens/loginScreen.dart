e0ddbb584a098fac0db3fda7ad33bd75b70b4b15
Prabhneet Narula <imailcode@gmail.com>
2022-02-10 22:27:44 +0530

    Some Changes Done.

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/textFieldInput.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +29,26 @@ class _LoginScreenState extends State<LoginScreen> {
       padding: const EdgeInsets.symmetric(horizontal: 32),
       width: double.infinity,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Flexible(child: Container(), flex: 2),
           //svg image,
           SvgPicture.asset('lib/assets/images/Instagram.svg',
               color: primaryColor, height: 64),
           const SizedBox(height: 64),
           //TextField for email,
+          TextFieldInput(
+              textEditingController: _emailController,
+              hintText: "Enter your email",
+              textInputType: TextInputType.emailAddress),
           //TextField for password,
+          TextFieldInput(
+            textEditingController: _passwordController,
+            hintText: "Enter your password",
+            textInputType: TextInputType.text,
+            isPass: true,
+          )
           //Button for login,
           //Transtioning for SignUp
         ],
